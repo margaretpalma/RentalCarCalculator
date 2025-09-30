71c59fbb19d73a5744254ad0a209643b92ace113
margaretpalma <mpalma1496@gmail.com>
2025-09-30 08:55:37 -0400

    create project and variables

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+
+        //what are my known values?
+        double priceOfBasicCarRentalPerDay = 29.99;
+        double priceOfOptionTollTag = 3.95;
+        double priceofOptionGPS = 2.95;
+        double priceofOptionRoadsideAssistance = 3.95;
+        float  percentSurchargeForUnderage = 0.30f;
+        int userageLimit = 25;
+
+        //what values do i need to get from the user?
+        String pickupDate;
+        int numberOfDays;
+        boolean optionTollTag;
+        boolean optionGPS;
+        boolean optionRoadsideAssistance;
+        int age;
+
+        //what needs to be calculated?
+        double basicCarRentalAmount;
+        double optionsAmount;
+        double underageSurcharge;
+        double totalCost;
+
+        //display the results
+    }
+}
