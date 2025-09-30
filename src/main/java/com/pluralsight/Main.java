4290eb97c8b2a6fa6c1859617bb46bb263371226
margaretpalma <mpalma1496@gmail.com>
2025-09-30 09:34:56 -0400

    added more code

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,10 +1,10 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-
+        Scanner scanner = new Scanner(System.in);
 
         //what are my known values?
         double priceOfBasicCarRentalPerDay = 29.99;
@@ -14,20 +14,39 @@ public class Main {
         float  percentSurchargeForUnderage = 0.30f;
         int userageLimit = 25;
 
+
         //what values do i need to get from the user?
-        String pickupDate;
-        int numberOfDays;
-        boolean optionTollTag;
-        boolean optionGPS;
-        boolean optionRoadsideAssistance;
-        int age;
+        System.out.print("What is the pickup date? ");
+        String pickupDate = scanner.nextLine();
+
+        System.out.print("How many days? ");
+        int numberOfDays = scanner.nextInt();
+        scanner.nextLine();                //clears the CRLF
+
+        System.out.print("Do you want toll tag? Y/N ");
+        boolean optionTollTag = scanner.nextLine().equalsIgnoreCase("y");  //.equalsIgnoreCase
+
+        System.out.print("Do you want GPS? Y/N ");
+        boolean optionGPS = scanner.nextLine().equalsIgnoreCase("y");  //.equalsIgnoreCase
+
+        System.out.print("Do you want roadside Assistance? Y/N ");
+        boolean optionRoadsideAssistance = scanner.nextLine().equalsIgnoreCase("y");  //.equalsIgnoreCase
+
+        System.out.print("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();         //clears CRLF
 
         //what needs to be calculated?
-        double basicCarRentalAmount;
-        double optionsAmount;
-        double underageSurcharge;
-        double totalCost;
+        double basicCarRentalAmount = 0;
+        double optionsAmount = 0;
+        double underageSurcharge = 0;
+        double totalCost = 0;
 
         //display the results
+        System.out.println("Quote for rental: " );
+        System.out.printf("Basic Car Rental:    $%.2f\n", basicCarRentalAmount);
+        System.out.printf("Options:             $%.2f\n", optionsAmount);
+        System.out.printf("Underage Surcharge:  $%.2f\n", underageSurcharge);
+        System.out.printf("Total:               $%.2f\n", totalCost);
     }
 }
